1a7ad7390a584ac5a719a23e865c1b2bd97ecc9b
Caio <caiopadilha@id.uff.br>
2022-10-17 15:33:52 -0300

    Requisitos 24 e 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,18 @@
+db.produtos.updateOne(
+    { valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: {
+      $gt: 40,
+        },
+      },
+    } },
+    {
+      $push: {
+        tags: {
+      $each: ["muito sódio"],
+        },
+      },
+    },
+  );
+  db.produtos.find({}, { nome: true, tags: true, _id: false });
